309b385ab8c0d8320296b0ed055179cecadb37dd
HDOlsen <38845610+HDOlsen@users.noreply.github.com>
2018-06-13 23:45:01 -0500

    Add files via upload

--- trips.js
+++ trips.js
@@ -2,6 +2,14 @@
 // loading the module
 const express = require('express')
 const app = express()
+var session = require('express-session')
+
+// setting up middleware to use the session
+app.use(session({
+  secret: 'guest',
+  resave: true,
+  saveUninitialized: true
+}))
 
 // body parser for parsing JSON
 var bodyParser = require('body-parser')
@@ -14,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 let trips = []
+let users = []
 
 // this means localhost:3000/site.css will work
 app.use(express.static('style'))
@@ -25,7 +34,85 @@ var mustacheExpress = require('mustache-express');
 app.engine('mustache', mustacheExpress())
 
 app.set('view engine', 'mustache')
+
 app.set('views', './views')
+
+
+//Working with Registration and Authentication
+
+app.get('/login',function(req,res){
+  res.render('login')
+})
+
+app.get('/profile',function(req,res){
+  console.log(req.session.usernameReg)
+  console.log(req.session.Logout)
+  res.render('profile',{usernameReg : req.session.usernameReg, username : req.session.username, Logout: req.session.Logout})
+  if(req.session.out) {
+    res.redirect('/login')
+    res.session.cookie.expires 
+  } else {
+    res.render('profile')
+  }
+
+
+})
+
+app.post('/profile',function(req,res){
+  console.log('profile')
+  let usernameReg  = req.body.usernameReg
+  let passwordReg = req.body.passwordReg
+  let username  = req.body.username
+  let password = req.body.password
+  let Logout = "Logout"
+  let out = req.body.out
+
+  if(req.session) {
+    req.session.usernameReg = usernameReg
+    req.session.passwordReg = passwordReg
+    req.session.username = username
+    req.session.password = password
+    req.session.Logout = Logout
+    req.session.out = out
+     //setting the expiration date of the cookies so we can
+     //30 minute expiry time
+    var time = 300000
+    req.session.cookie.expires = new Date(Date.now() + time)
+    req.session.cookie.maxAge = time
+
+
+
+}
+
+  // go to the profile page
+  res.redirect('/profile')
+
+})
+
+//Working with Login 
+
+
+
+
+function validateLogin(req,res,next) {
+
+  if(req.session.username) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+
+}
+
+//Access to all pages
+app.all('/views/*',validateLogin,function(req,res,next){
+  next()
+
+})
+
+
+//Working with Trip Post
+
 
 app.get('/',function(req,res){
     res.render('views', {title: "title"})
@@ -34,16 +121,14 @@ app.get('/',function(req,res){
   app.post('/trips',function(req,res){
   
     let title = req.body.title
-    trips.push({title : title})
 
     let image = req.body.image
-    trips.push({image : image.jpg})
 
     let departure = req.body.departure
-    trips.push({departure : departure})
 
     let arrival = req.body.arrival
-    trips.push({arrival : arrival})
+
+    trips.push({title : title, image : image, departure : departure, arrival : arrival})
 
     res.render('views', {tripsList : trips})
   
